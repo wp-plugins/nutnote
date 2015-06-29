76a7658c23347c631bb38fb488a877accc3eb393
nutnote <nutnote@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-06-29 07:19:04 +0000

    uncategories posts shown
    git-svn-id: https://plugins.svn.wordpress.org/nutnote/trunk@1189364 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- singletons/introspector.php
+++ singletons/introspector.php
@@ -75,9 +75,9 @@ class JSON_API_Introspector {
         $wp_categories = get_categories( $args );
         $categories    = array();
         foreach ( $wp_categories as $wp_category ) {
-            if ( $wp_category->term_id == 1 && $wp_category->slug == 'uncategorized' ) {
-                continue;
-            }
+            // if ( $wp_category->term_id == 1 && $wp_category->slug == 'uncategorized' ) {
+            //    continue;
+            //}
             $categories[] = $this->get_category_object( $wp_category );
         }
 
